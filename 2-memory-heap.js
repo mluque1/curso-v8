3846c9a130d5a9f7d394960256bc85cab92a92ec
Miguel Luque <migue.luque.2014@gmail.com>
2021-06-04 22:19:37 -0500

    2 - Memory Heap

---
+++ 2-memory-heap.js
@@ -0,0 +1,10 @@
+//Destina un espacio a la variable para guardar su valor.
+var nombre = 'Miguel';
+//Detecta otro tipo de variable.
+const edad = 24;
+
+//Detecta el objeto, guarda el objeto y sus valores en otro espacio del memory heap
+const carro = {
+    marca: 'Toyota',
+    modelo: '2020'
+}
