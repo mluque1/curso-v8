e2aabccb5686ff6239e2fa4217d5042d55badfc0
Miguel Luque <migue.luque.2014@gmail.com>
2021-06-04 22:20:11 -0500

    4 - Asincronia

---
+++ 4-asincronia.js
@@ -0,0 +1,12 @@
+//Call Stack
+console.log('Taco 1');
+console.log('Taco 2');
+console.log('Taco 3');
+
+//Web APIs
+setTimeout(() => {
+    console.log('Torta');
+}, 0)
+
+//Call Stack
+console.log('Taco 4')
